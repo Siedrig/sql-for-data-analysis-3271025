57a7f8d754727be89070dd9819e7080b10aed2bb
Siedrig <112951304+Siedrig@users.noreply.github.com>
2025-05-26 18:13:48 +0000

    String Functions

--- my queries/OOGA BOOGA.sql
+++ my queries/OOGA BOOGA.sql
@@ -1,3 +1,15 @@
-SELECT *
-FROM Orders
-Where YEAR(CreationDate) > (YEAR(now()) - 10);
+SELECT TRIM(
+    'M'
+    FROM ProductCode
+  ) AS TrimmedProductCode,
+  P.ProductCode,
+  REPLACE(ProductCode, 'BLU', 'BLE') as NewProductCode,
+  CONCAT(FirstName, ' ', LastName) AS FullName,
+  UPPER(
+    CONCAT(Address, ' ', City, ' ', State, ' ', Zipcode)
+  ) AS FullAddress
+FROM Customer C
+  JOIN Orders O ON C.CustomerId = O.CustomerId
+  JOIN OrderItem OI ON O.OrderID = OI.OrderID
+  JOIN Product P ON OI.ProductID = P.ProductID
+  WHERE P.ProductCode LIKE '%BLU%';
