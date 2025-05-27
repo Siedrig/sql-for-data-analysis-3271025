d57197969eae5bed3ac95b4fb2576426a9701cf2
Siedrig <112951304+Siedrig@users.noreply.github.com>
2025-05-27 19:11:08 +0000

    yes

--- Chapter 5/Create View V_Orders.sql
+++ Chapter 5/Create View V_Orders.sql
@@ -2,8 +2,9 @@ CREATE OR REPLACE VIEW V_Orders AS
 SELECT O.CreationDate AS OrderDate,
   CONCAT(S.FirstName, ' ', S.LastName) AS Salesperson,
   COUNT(1) AS 'Amount of Orders',
-  SUM(TotalDue) AS 'Total Due'
+  SUM(TotalDue) AS 'TotalDue'
 FROM Orders O
   join Salesperson S ON(O.SalespersonID = S.SalespersonID)
 GROUP BY O.CreationDate,
-  concat(S.FirstName, ' ', S.LastName)
+  concat(S.FirstName, ' ', S.LastName);
+  commit;
